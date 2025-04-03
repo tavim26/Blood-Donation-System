3450d5acd8efae1cb2d393f325c9d87267c201cc
Tavi Mirisan <146010448+tavim26@users.noreply.github.com>
2025-04-03 16:05:29 +0300

    Delete FlaskProject/config.py

--- FlaskProject/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-class Config:
-    BASEDIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///C:/Users/tavim/Desktop/Projects/Blood-Donation-System/FlaskProject/database.sqlite'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'my_secret_key')
